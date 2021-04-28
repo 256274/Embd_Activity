998532d4f137fb27680ccf3f854b69e7bf7751fd
256274 <sumanthreddy83@gmail.com>
2021-04-28 15:36:23 +0530

    added activities 2 and 3

--- SeatHeatingApp.c
+++ SeatHeatingApp.c
@@ -1,7 +1,19 @@
 #include "activity1.h"
+#include "activity2.h"
+#include "activity3.h"
+#include <util/delay.h>
+#include <avr/io.h>
 
 int main()
 {
     Glow_LED();
+    initADC();
+    uint16_t temp;
+    while(1)
+    {
+        temp = Read_TEMP(0);
+        _delay_ms(200);
+    }
+    Temp_Level();
     return 0;
 }
