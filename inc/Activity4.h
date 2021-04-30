9522028031644c7c0a95118239af66112eb04ec3
256274 <sumanthreddy83@gmail.com>
2021-04-30 20:49:14 +0530

    Modified act 4

--- inc/Activity4.h
+++ inc/Activity4.h
@@ -3,18 +3,29 @@
 
 /** \brief initialize the UART communication
  *
- * \param ubrr_value
- * \param
- * \return
+ * \param ubrr_value 
+ * \return no return
  *
  */
 
 
 void USARTInit(uint16_t ubrr_value);
 
+/** \brief reads the temperature value
+ *
+ * \param no parameter
+ * \return returns a character
+ *
+ */
 
 char USARTReadChar();
 
+/** \brief Writes a value to the MCU
+ *
+ * \param data, value that is to be displayed
+ * \return no return
+ *
+ */
 
 void USARTWriteChar(uint8_t data);
 
